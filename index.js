8c33f5d5ff9712bc33f6e42f25989287fb368e27
Fukki <takungwong@gmail.com>
2018-12-30 15:00:40 +0700

    :x

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-const fs = require('fs')
-const path = require('path')
-module.exports = function saveMIRR(dispatch) {
-	let teleporters = null;
-	fs.readFile(path.join(__dirname, 'teleporters.json'), (err, data) => {
-		teleporters = JSON.parse(data);
-		return;
-	});
-	dispatch.hook('S_SPAWN_NPC', 8, (event) => {
-		if(!event.villager) return;
-		for(let i of teleporters){
-			if(!(i.HuntingZoneId - event.huntingZoneId) && !(i.ID - event.templateId)){
-				event.huntingZoneId = 152;
-				event.templateId = 6014;
-				return true;
-			}
+module.exports = function saveMIRR(mod) {
+	const teleporters = [
+		{"HuntingZoneId": 13, "ID": 5012},
+		{"HuntingZoneId": 58, "ID": 1005},
+		{"HuntingZoneId": 59, "ID": 1017},
+		{"HuntingZoneId": 60, "ID": 1030},
+		{"HuntingZoneId": 61, "ID": 1009},
+		{"HuntingZoneId": 62, "ID": 1027},
+		{"HuntingZoneId": 64, "ID": 1050},
+		{"HuntingZoneId": 65, "ID": 1014},
+		{"HuntingZoneId": 66, "ID": 1015},
+		{"HuntingZoneId": 67, "ID": 1022},
+		{"HuntingZoneId": 68, "ID": 1015},
+		{"HuntingZoneId": 69, "ID": 1005},
+		{"HuntingZoneId": 70, "ID": 1007},
+		{"HuntingZoneId": 71, "ID": 1007},
+		{"HuntingZoneId": 73, "ID": 1007},
+		{"HuntingZoneId": 74, "ID": 1021},
+		{"HuntingZoneId": 75, "ID": 1015},
+		{"HuntingZoneId": 76, "ID": 1007},
+		{"HuntingZoneId": 77, "ID": 1008},
+		{"HuntingZoneId": 78, "ID": 1007},
+		{"HuntingZoneId": 79, "ID": 1008},
+		{"HuntingZoneId": 80, "ID": 1008},
+		{"HuntingZoneId": 81, "ID": 1008},
+		{"HuntingZoneId": 83 ,"ID": 1007},
+		{"HuntingZoneId": 85, "ID": 1008},
+		{"HuntingZoneId": 833, "ID": 1021}
+	];
+	
+	mod.hook('S_SPAWN_NPC', 10, e => {
+		if(!e.villager) return;
+		if (teleporters.find(o => o.HuntingZoneId === e.huntingZoneId && o.ID === e.templateId)) {
+			e.huntingZoneId = 152;
+			e.templateId = 6014;
+			return true;
 		}
 	});
 }
